624b9671b0a685124c44bddf48d1a1b6cd38918b
brodao <brodao@gmail.com>
2020-11-21 16:44:53 -0300

    Reorganização de repositórios e projetos associados.

--- src/logger.ts
+++ src/logger.ts
@@ -9,175 +9,161 @@ let _oraSpinner: any;
 let _printNewLineBeforeNextLog = false;
 
 function _maybePrintNewLine() {
-	if (_printNewLineBeforeNextLog) {
-		_printNewLineBeforeNextLog = false;
-		console.log();
-	}
+  if (_printNewLineBeforeNextLog) {
+    _printNewLineBeforeNextLog = false;
+    console.log();
+  }
 }
 
 function consoleLog(...args: any[]) {
-	_maybePrintNewLine();
-	console.log(...args);
+  _maybePrintNewLine();
+  console.log(...args);
 }
 
 function consoleWarn(...args: any[]) {
-	_maybePrintNewLine();
-	console.warn(...args);
+  _maybePrintNewLine();
+  console.warn(...args);
 }
 
 function consoleError(...args: any[]) {
-	_maybePrintNewLine();
-	console.error(...args);
+  _maybePrintNewLine();
+  console.error(...args);
 }
 
 function respectProgressBars(commitLogs: () => void) {
-	if (_bundleProgressBar) {
-		_bundleProgressBar.terminate();
-		_bundleProgressBar.lastDraw = '';
-	}
-	if (_oraSpinner) {
-		_oraSpinner.stop();
-	}
-	commitLogs();
+  if (_bundleProgressBar) {
+    _bundleProgressBar.terminate();
+    _bundleProgressBar.lastDraw = '';
+  }
+  if (_oraSpinner) {
+    _oraSpinner.stop();
+  }
+  commitLogs();
 
-	if (_bundleProgressBar) {
-		_bundleProgressBar.render();
-	}
-	if (_oraSpinner) {
-		_oraSpinner.start();
-	}
+  if (_bundleProgressBar) {
+    _bundleProgressBar.render();
+  }
+  if (_oraSpinner) {
+    _oraSpinner.start();
+  }
 }
 
 function getPrefix(chalkColor: Color) {
-	return chalkColor(`[${new Date().toTimeString().slice(0, 8)}]`);
+  return chalkColor(`[${new Date().toTimeString().slice(0, 8)}]`);
 }
 
 function withPrefixAndTextColor(args: any[], chalkColor: Color = chalk.gray) {
-	if (program.nonInteractive) {
-		return [getPrefix(chalkColor), ...args.map(arg => chalkColor(arg))];
-	} else {
-		return args.map(arg => chalkColor(arg));
-	}
+  if (program.nonInteractive) {
+    return [getPrefix(chalkColor), ...args.map((arg) => chalkColor(arg))];
+  } else {
+    return args.map((arg) => chalkColor(arg));
+  }
 }
 
 function withPrefix(args: any[], chalkColor = chalk.gray) {
-	if (program.nonInteractive) {
-		return [getPrefix(chalkColor), ...args];
-	} else {
-		return args;
-	}
+  if (program.nonInteractive) {
+    return [getPrefix(chalkColor), ...args];
+  } else {
+    return args;
+  }
 }
 
 function log(...args: any[]) {
-	if (log.config.raw) {
-		return;
-	}
+  if (log.config.raw) {
+    return;
+  }
 
-	respectProgressBars(() => {
-		consoleLog(...withPrefix(args));
-	});
+  respectProgressBars(() => {
+    consoleLog(...withPrefix(args));
+  });
 }
 
-log.nested = function(message: any) {
-	respectProgressBars(() => {
-		consoleLog(message);
-	});
+log.nested = function (message: any) {
+  respectProgressBars(() => {
+    consoleLog(message);
+  });
 };
 
 log.newLine = function newLine() {
-	respectProgressBars(() => {
-		consoleLog();
-	});
+  respectProgressBars(() => {
+    consoleLog();
+  });
 };
 
 log.printNewLineBeforeNextLog = function printNewLineBeforeNextLog() {
-	_printNewLineBeforeNextLog = true;
+  _printNewLineBeforeNextLog = true;
 };
 
 log.error = function error(...args: any[]) {
-	if (log.config.raw) {
-		return;
-	}
+  if (log.config.raw) {
+    return;
+  }
 
-	respectProgressBars(() => {
-		consoleError(...withPrefixAndTextColor(args, chalk.red));
-	});
+  respectProgressBars(() => {
+    consoleError(...withPrefixAndTextColor(args, chalk.red));
+  });
 };
 
 log.warn = function warn(...args: any[]) {
-	if (log.config.raw) {
-		return;
-	}
+  if (log.config.raw) {
+    return;
+  }
 
-	respectProgressBars(() => {
-		consoleWarn(...withPrefixAndTextColor(args, chalk.yellow));
-	});
+  respectProgressBars(() => {
+    consoleWarn(...withPrefixAndTextColor(args, chalk.yellow));
+  });
 };
 
-log.gray = function(...args: any[]) {
-	if (log.config.raw) {
-		return;
-	}
+log.gray = function (...args: any[]) {
+  if (log.config.raw) {
+    return;
+  }
 
-	respectProgressBars(() => {
-		consoleLog(...withPrefixAndTextColor(args));
-	});
+  respectProgressBars(() => {
+    consoleLog(...withPrefixAndTextColor(args));
+  });
 };
 
-log.raw = function(...args: any[]) {
-	if (!log.config.raw) {
-		return;
-	}
+log.raw = function (...args: any[]) {
+  if (!log.config.raw) {
+    return;
+  }
 
-	respectProgressBars(() => {
-		consoleLog(...args);
-	});
+  respectProgressBars(() => {
+    consoleLog(...args);
+  });
 };
 
 log.verbose = (text: string | string[], args?: any) => {
-	if (args) {
-		log.warn(text);
-		Object.keys(args).forEach(key => {
-			log.nested(`  ${key} ${chalk.bold(args[key])}`);
-		});
-	} else {
-		log.gray(text);
-	}
+  if (args) {
+    log.warn(text);
+    Object.keys(args).forEach((key) => {
+      log.nested(`  ${key} ${chalk.bold(args[key])}`);
+    });
+  } else {
+    log.gray(text);
+  }
 };
 
 log.chalk = chalk;
 
 log.config = {
-	raw: false,
+  raw: false,
 };
 
 log.showBanner = (appInfo: any) => {
-	log.newLine();
+  log.newLine();
 
-	log.gray(
-		'/====================v======================================================\\'
-	);
-	log.gray(
-		'|     /////// ////// | AC FERRAMENTAS - Extensões para VS-Code e Node.JS    |'
-	);
-	log.gray(
-		'|    //   // //      | (C) 2020 Alan Candido (BRODAO) <brodao@gmail.com>    |'
-	);
-	log.gray(
-		'|   /////// //       |                                                      |'
-	);
-	log.gray(
-		'|  //   // //        | Ferramentas de apoio a desenvolvedores TOTVS         |'
-	);
-	log.gray(
-		'| //   // //////     | https://github.com/brodao/workspace/projects/AFPV    |'
-	);
-	log.gray(
-		'\\====================^======================================================/'
-	);
-	log.gray(`${appInfo.name} [${appInfo.version}] ${appInfo.description}`);
+  log.gray('/====================v======================================================\\');
+  log.gray('|     /////// ////// | AC FERRAMENTAS - Extensões para VS-Code e Node.JS    |');
+  log.gray('|    //   // //      | (C) 2020 Alan Candido (BRODAO) <brodao@gmail.com>    |');
+  log.gray('|   /////// //       |                                                      |');
+  log.gray('|  //   // //        | Ferramentas de apoio a desenvolvedores TOTVS         |');
+  log.gray('| //   // //////     | https://github.com/brodao/workspace/projects/AFPV |');
+  log.gray('\\====================^======================================================/');
+  log.gray(`${appInfo.name} [${appInfo.version}] ${appInfo.description}`);
 
-	log.newLine();
+  log.newLine();
 };
 
 export default log;
